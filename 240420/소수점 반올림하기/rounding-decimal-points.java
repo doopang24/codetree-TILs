277c4b38e4dde4a838a4e1536494f8a9a640a200
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-04-20 14:07:44 +0900

    Title: 소수점 반올림하기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240420/소수점 반올림하기/rounding-decimal-points.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double a = 25.352;
+        double b = (double)Math.round(a*10)/10;
+        System.out.printf("%.1f%n", b);
+    }
+}
