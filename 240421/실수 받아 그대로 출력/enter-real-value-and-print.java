d5adbfcfdb7d60404c7f59c4eb47acf6addf9341
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-04-21 23:27:20 +0900

    Title: 실수 받아 그대로 출력, Time: 234ms, Memory: 11MB, Status: Passed - CodeTree

--- 240421/실수 받아 그대로 출력/enter-real-value-and-print.java
+++ 240421/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -5,6 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         double n = sc.nextDouble();
-        System.out.printf("%.2f", (double)Math.round(n*100)/100);
+        System.out.printf("%.2f", n);
+        //System.out.printf("%.2f", (double)Math.round(n*100)/100);
     }
 }
