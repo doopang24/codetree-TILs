7e411be284ece735215bb57d1d04af8308d601f2
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-04-28 14:14:02 +0900

    Title: 전화번호 바꾸기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240428/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        String a = sc.next();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(a+"-"+y+"-"+x);
+    }
+}
