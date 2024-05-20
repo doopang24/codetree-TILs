1d37a3592caecd1943a26c42e9070fcdda82a317
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-05-20 19:53:13 +0900

    Title: a/b 출력, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240520/a/b 출력/a-divide-b.java
+++ 240520/a/b 출력/a-divide-b.java
@@ -5,17 +5,14 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        double b = sc.nextDouble();
-        double c = a/b;
+        int b = sc.nextInt();
 
-       // for(int i=0; i<20; i++) {
-      //      c /= 10;
-       // }
+        System.out.print(a/b + ".");
+        a = (a%b)*10;
 
-       // for(int i=0; i<20; i++) {
-       //     c *= 10;
-      //  }
-
-        System.out.printf("%22.20f", c);
+        for(int i=0; i<20; i++) {
+            System.out.print(a/b);
+            a = (a%b)*10;
+        }
     }
 }
