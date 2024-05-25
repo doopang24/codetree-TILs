c803192930c74ccb74916ddb1b655055e8dcf300
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-05-25 16:25:22 +0900

    Title: 1까지 나누기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/1까지 나누기/divide-by-1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=1; i<=5000; i++) {
+            n /= i;
+            cnt++;
+            if(n<=1) break;
+        }
+        System.out.println(cnt);
+    }
+}
