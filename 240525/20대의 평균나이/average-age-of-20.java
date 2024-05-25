bc7d21f85fd02e8def251f6664ff231f488153bf
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-05-25 18:07:34 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240525/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n, cnt = 0, sum = 0;
+        double a;
+
+        while(true) {
+            n = sc.nextInt();
+            if(n>=30) break;
+            sum += n;
+            cnt++;
+        }
+        a = (double)sum/cnt;
+        System.out.printf("%.2f", a);
+    }
+}
