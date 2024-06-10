b5d55b12c60357367694e469260a0fb81623ff12
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-10 22:10:15 +0900

    Title: 별표 출력하기 11, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<=2*n; i++){
+            for(int j=0; j<=2*n; j++){
+                if(i%2==1 && j%2==1) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
