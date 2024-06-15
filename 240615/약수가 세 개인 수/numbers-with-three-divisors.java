0783a341e84f1c507afb69617096e603a7a30f5a
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-15 22:35:09 +0900

    Title: 약수가 세 개인 수, Time: 232ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240615/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end   = sc.nextInt();
+        int cnt = 0;
+
+        for(int i=start; i<=end; i++) {
+            int divirsor = 0;
+
+            for(int j=1; j<i; j++) {
+                if(i%j==0) divirsor++;
+            }
+            if(divirsor==3) cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
