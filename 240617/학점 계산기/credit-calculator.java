f20d710e3a27c4f2fb56f201c82b04db3f280794
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-17 23:24:24 +0900

    Title: 학점 계산기, Time: 169ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240617/학점 계산기/credit-calculator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum = 0;
+        double avr;
+
+        for(int i=0; i<n; i++) {
+            double a = sc.nextDouble();
+            sum += a;
+        }
+        avr = sum/n;
+        System.out.printf("%.1f%n",avr);
+
+        if(avr>=4.0) System.out.println("Perfect");
+        else if(avr>=3.0) System.out.println("Good");
+        else System.out.println("Poor");
+    }
+}
