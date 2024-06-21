7b164bbb5c19f9f87a059434c66b809fbf11df88
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-21 17:36:53 +0900

    Title: 배열의 특정 위치 찾기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240621/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240621/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -6,8 +6,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
         int sum = 0; 
-        int sum3 = 0, cnt = 0;
-        double avr;
+        int sum3 = 0;
+        double avr, cnt=0.0;
 
         for(int i=0; i<10; i++) {
             arr[i] = sc.nextInt();
