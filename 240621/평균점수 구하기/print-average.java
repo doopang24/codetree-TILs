9f42f8da42cab6284a4da876da394b243e32382a
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-21 14:52:50 +0900

    Title: 평균점수 구하기, Time: 142ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240621/평균점수 구하기/print-average.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double sum, avr;
+        sum = 0.0;
+
+        for(int i=0; i<8; i++) {
+            double n = sc.nextDouble();
+            sum += n;
+        }
+        avr = sum/8;
+
+        System.out.printf("%.1f",avr);
+    }
+}
