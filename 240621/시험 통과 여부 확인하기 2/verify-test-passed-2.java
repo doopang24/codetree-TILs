d857e4758298cea02a49cd164701d8211f6e2c14
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-21 15:32:01 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240621/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum, cnt;
+        cnt = 0;
+
+        for(int i=0; i<n; i++) {
+            sum=0;
+            for(int j=0; j<4; j++) {
+                int a = sc.nextInt();
+                sum += a; 
+            }
+            if((sum/4)>=60) {
+                cnt++;
+                System.out.println("pass");
+            } else {
+                System.out.println("fail");
+            }
+        }
+        System.out.println(cnt);
+    }
+}
