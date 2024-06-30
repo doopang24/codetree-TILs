edb107d81bbf3b38fa6a219fc7343f322215617b
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-30 15:34:59 +0900

    Title: 코로나 메뉴얼2, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240630/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
+        // int cntA, cntB, cntC, cntD;
+        // cntA = 0;
+        // cntB = 0;
+        // cntC = 0;
+        // cntD = 0;
+
+        for(int i=0; i<3; i++) {
+            char c = sc.next().charAt(0);
+            int tem = sc.nextInt();
+
+            if(c=='Y') {
+                if(tem>=37) arr[0]++;
+                else arr[1]++;
+            } else {
+                if(tem>37) arr[2]++;
+                else arr[3]++;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++) System.out.print(arr[i]+" ");
+        if(arr[0]>=2) System.out.print("E");
+    }
+}
