dade861b0dc98fc49b926d9b6ddc5a7cb1eb8c19
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-06-30 19:01:42 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240630/연속부분수열일까/contiguous-array-or-not.java
+++ 240630/연속부분수열일까/contiguous-array-or-not.java
@@ -5,25 +5,31 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
-        int[] arr1 = new int[n1];
         int n2 = sc.nextInt();
+        int[] arr1 = new int[n1];
         int[] arr2 = new int[n2];
         int dif = 0;
 
         for(int i=0; i<n1; i++) {
             arr1[i] = sc.nextInt();
         }
-
         for(int i=0; i<n2; i++) {
             arr2[i] = sc.nextInt();
         }
 
-        for(int i=0; i<n1; i++) {
-            if(arr1[i]==arr2[0]) {
-                dif = i;
-                break;
+        for(int i=0; i<n2; i++) {
+            for(int j=0; j+i<n1; j++) {
+                if(arr2[i]==arr1[i+j]) dif = j;
             }
         }
+
+        // for(int i=0; i<n1; i++) {
+        //     if(arr1[i]==arr2[0]) {
+        //         dif = i;
+        //         break;
+        //     }
+        // }
+
         boolean match = true;
         for(int i=0; i<n2; i++) {
             if(arr2[i]!=arr1[i+dif]) {
