f43934ea76744cbbd128692b333f8106b788f17c
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-07-15 15:26:03 +0900

    Title: 문자열 범위 출력하기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String result = str.substring(2,10);
+        System.out.print(result);
+    }
+}
