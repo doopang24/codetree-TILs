961a29afc9843a9fe4ac478c80863dfb1c6c1552
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-07-29 21:02:07 +0900

    Title: 첫 번째와 두 번째 교환, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240729/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        char fir = arr[0];
+        char sec = arr[1];
+
+        for(int i=0; i<str.length(); i++) {
+            if(arr[i]==fir) arr[i] = sec;
+            else if(arr[i]==sec) arr[i] = fir;
+        }
+
+        System.out.print(String.valueOf(arr));
+    }
+}
