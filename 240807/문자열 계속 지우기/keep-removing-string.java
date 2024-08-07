0930747c45dbfd88e69520e99be65d7bfd42e2a7
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-08-07 17:16:25 +0900

    Title: 문자열 계속 지우기, Time: 3026ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240807/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        int b = B.length();
+
+        while(A.contains(B)) {
+            for(int i=0; i<A.length()-b; i++) {
+                if(A.substring(i,i+b).equals(B)) {
+                    A = A.substring(0,i) + A.substring(i+b,A.length());
+                } 
+            }
+        }
+        System.out.print(A);
+    }
+}
