0fdb99188307a0669de7bfc1a4117b4c7792143c
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-08-15 13:03:36 +0900

    Title: 소문자와 숫자, Time: 116ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240815/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String result = "";
+
+        for(int i=0; i<str.length(); i++) {
+            if('1'<=str.charAt(i) && str.charAt(i)<='9') {
+                result += str.charAt(i);
+            } else if('A'<=str.charAt(i) && str.charAt(i)<='Z') {
+                char word = str.charAt(i);
+                result += (char)(word - 'A' + 'a');
+            } else if('a'<=str.charAt(i) && str.charAt(i)<='z') {
+                result += str.charAt(i);
+            }
+        }
+
+        System.out.print(result);
+    }
+}
