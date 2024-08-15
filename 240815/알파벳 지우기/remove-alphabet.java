525aaea2433a80a34a25fff023efd64aab42ce26
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-08-15 16:35:17 +0900

    Title: 알파벳 지우기, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240815/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+
+        String a = "", b = "";
+
+        for(int i=0; i<A.length(); i++) {
+            if('0'<=A.charAt(i) && A.charAt(i)<='9') {
+                a += A.charAt(i);
+            }
+        }
+        for(int i=0; i<B.length(); i++) {
+            if('0'<=B.charAt(i) && B.charAt(i)<'9') {
+                b += B.charAt(i);
+            }
+        }
+        System.out.print(Integer.parseInt(a) + Integer.parseInt(b));
+    }
+}
