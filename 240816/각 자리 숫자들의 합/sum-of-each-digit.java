640cd18c14057a7462bcbe3bfe8706c7d0384030
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-08-16 09:27:21 +0900

    Title: 각 자리 숫자들의 합, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240816/각 자리 숫자들의 합/sum-of-each-digit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String N = Integer.toString(n);
+
+        int sum = 0;
+        for(int i=0; i<N.length(); i++) {
+            int a = N.charAt(i) - '0';
+            sum += a;
+        }
+        System.out.print(sum);
+    }
+}
