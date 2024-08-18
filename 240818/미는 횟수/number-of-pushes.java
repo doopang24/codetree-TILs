c98ca6a442e9f34de6c65b96a7799c89319e3746
doopang24 <153696777+doopang24@users.noreply.github.com>
2024-08-18 19:22:56 +0900

    Title: 미는 횟수, Time: 107ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240818/미는 횟수/number-of-pushes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        int a = A.length();
+        String B = sc.next();
+        int cnt = 0;
+
+        for(int i=0; i<A.length(); i++) {
+            A = A.substring(a-1) + A.substring(0,a-1);
+            cnt++;
+            if(A.equals(B)) break;
+        }
+
+        if(cnt==0) System.out.print(-1);
+        else System.out.print(cnt);
+    }
+}
